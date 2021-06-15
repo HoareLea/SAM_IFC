808a25def8b9d455021c8cdb0b7bb7a09e000413
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-06-15 21:01:28 +0100

    SetIfcRelSpaceBoundary Initial Implementation (WIP)

---
+++ SAM_IFC/SAM.Analytical.IFC/Modify/SetIfcRelSpaceBoundary.cs
@@ -0,0 +1,10 @@
+﻿namespace SAM.Analytical.IFC
+{
+    public static partial class Modify
+    {
+        public static void SetIfcRelSpaceBoundary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
